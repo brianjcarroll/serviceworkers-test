95237724611fc4cab48c946881b63abf14f69f7c
Kevin Foley <kfoley@squarespace.com>
2014-07-09 18:49:36 -0400

    Feature detects.

--- js/sw.js
+++ js/sw.js
@@ -3,6 +3,19 @@
 (function () {
   if (navigator.serviceWorker) {
     console.log('This browser supports service workers.');
+
+    navigator.serviceWorker.register(
+      'main.js',
+      'sw.js',
+      '../index.html',
+      '../css/main.css',
+      '../css/normalize.css',
+      '..normalize.min.css'
+    ).then(
+      function (sw) {
+        console.log(sw);
+      }
+    )
   }
 }());
 
